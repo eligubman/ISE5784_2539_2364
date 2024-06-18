543fab22b95f96c13d1c977f460b621c30d6ffbc
Azriel-Ehrlich <129371825+Azriel-Ehrlich@users.noreply.github.com>
2024-06-18 16:29:23 +0300

    added a picture with more than 10 shapes

--- unittests/renderer/ReflectionRefractionTests.java
+++ unittests/renderer/ReflectionRefractionTests.java
@@ -189,4 +189,77 @@ public class ReflectionRefractionTests {
                 .setImageWriter(new ImageWriter("take104", 1000, 1000)).build().renderImage().writeToImage();
 
     }
+
+    @Test
+    public void myShape4() {
+
+        final Camera.Builder bonuscameraBuilder = Camera.getBuilder()
+                .setDirection(new Vector(1, 0, 0), new Vector(0, 0, 1));
+
+        scene.setAmbientLight(new AmbientLight(new Color(255, 255, 255).reduce(6), new Double3(0.15)));
+        scene.lights.add(new SpotLight(new Color(RED), new Point(-300, 6, 10), new Vector(1, 0, 0)));
+
+        double angle = 0;
+        double height = 0;
+
+        scene.geometries.add(new Plane(new Point(-4, 4, 0), new Vector(0, 0, 1))
+                .setMaterial(new Material().setKd(0.8).setKs(0.6).setShininess(100).setKt(0.7).setKr(0.5)));
+
+        java.awt.Color[] colors = { YELLOW, RED, ORANGE, BLUE };
+
+        for (int i = 25; i < 200; ++i) {
+            int colorIndex = i % colors.length;
+
+            scene.geometries
+                    .add(new Sphere(0.5, new Point(i / 25.0 * Math.cos(angle), i / 25.0 * Math.sin(angle), height))
+                            .setEmission(new Color(colors[colorIndex]).reduce(2))
+                            .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(80).setKt(0.3)));
+
+            angle += Math.PI / 15.0;
+            height += 0.15;
+        }
+
+        java.awt.Color[] colors2 = { BLUE, GREEN, PINK, BLACK, RED, GRAY };
+
+        height = 10;
+        for (int i = 25; i < 100; ++i) {
+            int colorIndex = i % colors2.length;
+
+            scene.geometries.add(new Sphere(0.3, new Point(i / 25.0 * Math.cos(angle), i * Math.sin(angle), height))
+                    .setEmission(new Color(colors2[colorIndex]).reduce(2))
+                    .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(80).setKt(0.3)));
+
+            angle += Math.PI / 30.0;
+            height += 0.5;
+        }
+
+        height = 10;
+        for (int i = 25; i < 300; ++i) {
+
+            scene.geometries.add(new Sphere(0.05, new Point(i / 25.0 * Math.cos(angle), i * Math.sin(angle), height))
+                    .setEmission(new Color(WHITE))
+                    .setMaterial(new Material().setKd(1).setKs(1d).setShininess(100).setKt(1d)));
+
+            angle += Math.PI / 60.0;
+            height += 0.1 % 50;
+        }
+
+        scene.lights.add(new SpotLight(new Color(255, 255, 255).reduce(2), new Point(-150, 0, 5), new Vector(1, 0, 0)));
+        scene.lights.add(new SpotLight(new Color(GREEN).reduce(2), new Point(50, 0, 5), new Vector(1, 0, 0)));
+
+        scene.setBackground(new Color(BLUE).reduce(TRANSLUCENT));
+
+        ImageWriter imageWriter = new ImageWriter("myShape4", 500, 500);
+
+        bonuscameraBuilder.setLocation(new Point(-330, 0, 5))
+                .setVpDistance(1000d)
+                .setVpSize(200, 200)
+                .setRayTracer(new SimpleRayTracer(scene))
+                .setImageWriter(imageWriter)
+                .build()
+                .renderImage()
+                .writeToImage();
+    }
+
+
 }
